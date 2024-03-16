b65bba8535e46e49041025f2b128366882426ac5
khalidbinibrahim <khalidbinibrahim01@gmail.com>
2024-03-16 13:18:06 +0600

    data fetching done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import './App.css'
+import { useEffect, useState } from 'react'
 import Navigation from './components/Navigation/navigation'
 import Header from './components/Header/header'
 import OurRecipes from './components/Our Recipes/ourRecipes'
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetch('recipe.json')
+      .then(res => {
+        if (!res.ok) {
+          throw new Error('Failed to fetch recipes');
+        }
+        return res.json();
+      })
+      .then(data => setRecipes(data));
+  }, []);
+
   return (
     <>
       <div className='px-36 py-14'>
